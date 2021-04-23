795026ce8cf6ad62ff57ea6e46324d7a0d60da9c
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-04-23 13:55:39 +0530

    adding_passing_query_string

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import queryString from 'query-string'; 
 
-const Posts = ({match}) => {
+const Posts =({match,location}) =>{
+  //const {sortBy, approved} = queryString.parse(location.search)
+  //console.log(soryBy, approved);
+  //const result =queryString.parse(location.search); 
+  const result = queryString.parse(location.search);  
+  console.log(result); 
   return (
     <div>
       <h1>Posts</h1>
-      Year:{match.params.year}  Month:{match.params.month}
+      {/* Year:{props.match.params.year}  Month:{props.match.params.month} */} 
+         Year:{match.params.year}, Month:{match.params.month}
     </div>
   );
 };
